c78c8b86a9fd80c28c846703ee43193dcc928841
JOHN MARK ENGANA <jhnmrkengana567@gmail.com>
2025-10-19 21:30:00 +0800

    Your update message here

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/portfolio/", // 👈 IMPORTANT — change this to your repo name
   server: {
     host: "::",
     port: 8080,
